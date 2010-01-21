cc4229136d2a300bf4584405d5c73bc05ea5e396
Steven <steven.jackson@morris.com>
2010-01-21 11:32:45 -0500

    added check for 301

--- sitemapper_0_1/source/pages/page_index.php
+++ sitemapper_0_1/source/pages/page_index.php
@@ -41,25 +41,17 @@
 			if(preg_match("/(\<loc\>)(http[^\<]*)/", $line, $match)){
 				// Curl Options
 				$url = $match[2];
-				$ch = curl_init($url);
-
-				// make sure we get the header
-				curl_setopt($ch, CURLOPT_HEADER, 1);
-				// make it a http HEAD request
-				curl_setopt($ch, CURLOPT_NOBODY, 1);
-				// add useragent
-				curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
-				//Tell curl to write the response to a variable
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-				// The maximum number of seconds to allow cURL functions to execute.
-				curl_setopt($ch,CURLOPT_CONNECTTIMEOUT, 2);
-				// Tell curl to stop when it encounters an error
-				curl_setopt($ch, CURLOPT_FAILONERROR, 1);
-			   
 				try{
-					$result = curl_exec($ch);
+					$chArray = get_url($url, 0); //0 for no redirect
 					// Testing for correct header response
-					if(!curl_errno($ch)){
+					echo $url . " | " . curl_getinfo($chArray["ch"], CURLINFO_HTTP_CODE) . "<br />";
+					if(curl_getinfo($chArray["ch"], CURLINFO_HTTP_CODE) != 301){
+						echo "<br />" . $chArray["header"]["url"] . " ***NO REDIR***<br />";
+					} else {
+						$chArray = get_url($url, 1); //1 for redirect
+						echo $chArray["header"]["url"] . " ***REDIR***<br />";
+					}
+					if(!curl_errno($chArray["ch"])){
 						//copy($tmpCache, $jpiimCache);
 						fwrite($fp, $url . ", 1\n");
 					} else {
@@ -92,3 +84,31 @@
 </form>
 </center>
 <?php include($VERSION_PATH . "core/footer.php"); ?> 
+<?php function get_url($url, $follow){
+	$ch = curl_init($url);
+
+	// make sure we get the header
+	curl_setopt($ch, CURLOPT_HEADER, 1);
+	// follow redirects
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $follow);
+	// make it a http HEAD request
+	curl_setopt($ch, CURLOPT_NOBODY, 1);
+	// add useragent
+	curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
+	//Tell curl to write the response to a variable
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	// The maximum number of seconds to allow cURL functions to execute.
+	curl_setopt($ch,CURLOPT_CONNECTTIMEOUT, 2);
+	// Tell curl to stop when it encounters an error
+	curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+	
+	//curl_exec($ch);
+	$content = curl_exec( $ch ); 
+    $err     = curl_errno( $ch ); 
+    $errmsg  = curl_error( $ch ); 
+    $header  = curl_getinfo( $ch ); 
+
+	$arr = array("ch" => $ch, "content" => $content, "err" => $err, "errmsg" => $errmsg, "header" => $header,);
+	
+	return $arr;
+}
